d8b7561c626807558b2c24f62a9d13017be43917
Nick Bobryk <nick.bobryk@vydia.com>
2018-10-27 12:55:03 -0400

    mouse-down

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-let canvas = document.getElementById('canvass')
-let context = canvas.getContext('2d')
+const canvas = document.getElementById('canvass')
+const context = canvas.getContext('2d')
 
 
 
 
 
 
+
+
+
+const onMouseDown = (e) => {
+  const rect = canvas.getBoundingClientRect()
+  const coordinates = {
+    x: e.x - rect.left + window.scrollX,
+    y: e.y + window.scrollX
+  }
+  console.log(e)
+  context.fillRect(coordinates.x, e.y, 2, 2)
+  console.log(coordinates)
+}
+
+canvas.addEventListener("mousedown", onMouseDown)
