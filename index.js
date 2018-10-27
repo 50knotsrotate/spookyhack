8c126a6eacef832efe28b4895b508cb42a786b1b
Nick Bobryk <nick.bobryk@vydia.com>
2018-10-27 15:37:41 -0400

    noise

--- index.js
+++ index.js
@@ -2,20 +2,16 @@ const canvas = document.getElementById('canvass')
 const context = canvas.getContext('2d')
 const pumpkinCoords = { x: 390, y: 300, r: 240 }
 let shapes = []
+noise.seed(0)
+let isDay = true
+let time = 0
 
 const checkPumpkinBounds = (x, y) => Math.sqrt((x-pumpkinCoords.x)*(x-pumpkinCoords.x) + (y-pumpkinCoords.y)*(y-pumpkinCoords.y)) < pumpkinCoords.r
 let currentShape = null
 
 
-const make_base = () => {
-  const base_image = new Image()
-  base_image.src = 'img/pumpkin.png'
-  base_image.onload = () => {
-    context.drawImage(base_image, 0, 0)
-  }
-}
-
-make_base()
+const base_image = new Image()
+base_image.src = 'img/pumpkin.png'
 
 const onMouseMove = (e) => {
   const rect = canvas.getBoundingClientRect()
@@ -66,7 +62,8 @@ const finishShape = () => {
 }
 
 const drawShapes = () => {
-  context.fillStyle = 'hsl(' + 360 * Math.random() + ', 50%, 50%)'
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  base_image.complete && context.drawImage(base_image, 0, 0)
   shapes.forEach((shape) => {
     context.beginPath()
     shape.forEach((coordinates, index, array) => {
@@ -77,6 +74,12 @@ const drawShapes = () => {
 
       if (index == array.length - 1) {
         context.closePath()
+        if (isDay) {
+          context.fillStyle = "#000"
+        } else {
+          time += 0.005
+          context.fillStyle = 'hsl(39, 100%, ' + (20 + noise.simplex2(time, 0) * 20) + '%)'
+        }
         context.fill()
         return
       }
@@ -96,6 +99,17 @@ const drawShapes = () => {
       context.lineTo(coordinates.x, coordinates.y)
       context.stroke()
     })
+  }
+}
+
+const onClickButton = () => {
+  isDay = !isDay
+  if (isDay) {
+    document.body.style.backgroundColor = "#fff"
+    canvas.style.backgroundColor = "#fff"
+  } else {
+    document.body.style.backgroundColor = "#000"
+    canvas.style.backgroundColor = "#000"
   }
 }
 
