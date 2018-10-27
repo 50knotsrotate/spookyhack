25660d2a457fab2f405ed8dc8519b1e8353ec159
Tyler Geerdts2 <tygee713@gmail.com>
2018-10-27 12:55:52 -0400

    Changed image to png and fixed canvas

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let canvas = document.getElementById('canvass')
 let context = canvas.getContext('2d')
 
+const make_base = () => {
+  base_image = new Image()
+  base_image.src = 'img/pumpkin.png'
+  base_image.onload = function(){
+    context.drawImage(base_image, 0, 0);
+  }
+}
 
-
-
-
-
+make_base()
