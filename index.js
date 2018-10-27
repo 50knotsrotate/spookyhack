595ef7d658eb5af6a46b97bfb1b8362f3eb18561
Tyler Geerdts2 <tygee713@gmail.com>
2018-10-27 13:50:12 -0400

    Added check circle bounds

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const canvas = document.getElementById('canvass')
 const context = canvas.getContext('2d')
+let base_image = new Image()
+base_image.src = 'img/pumpkin.png'
+let circle = { x: 390, y: 300, r: 240 }
+
+const checkCircleBounds = (x, y) => Math.sqrt((x-circle.x)*(x-circle.x) + (y-circle.y)*(y-circle.y)) < circle.r
 
 const make_base = () => {
-  base_image = new Image()
-  base_image.src = 'img/pumpkin.png'
   base_image.onload = function(){
+    context.beginPath()
+    context.arc(circle.x, circle.y, circle.r, 0, 2 * Math.PI)
+    context.stroke()
     context.drawImage(base_image, 0, 0);
   }
 }
 
 make_base()
+
+
 
 const onMouseDown = (e) => {
   const rect = canvas.getBoundingClientRect()
@@ -17,9 +25,8 @@ const onMouseDown = (e) => {
     x: e.x - rect.left + window.scrollX,
     y: e.y + window.scrollX
   }
-  console.log(e)
   context.fillRect(coordinates.x, e.y, 2, 2)
-  console.log(coordinates)
+  console.log(checkCircleBounds(e.x, e.y))
 }
 
 canvas.addEventListener("mousedown", onMouseDown)
